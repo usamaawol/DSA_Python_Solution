73dd76d3b0c8676a43c433a6b3dbc03778adf6e8
usamaawol <usamaawol0@gmail.com>
2026-06-30 11:42:16 +0300

    Add phase2 solutions

---
+++ phase2:4.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    color_count = {}
+
+    for color in ar:
+        color_count[color] = color_count.get(color, 0) + 1
+
+    pairs = 0
+    for count in color_count.values():
+        pairs += count // 2
+
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
